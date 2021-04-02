903751ceebfae269c77b86fab88e5991657f7fef
Ripon <habibkazi92@gmail.com>
2021-04-03 03:08:11 +0800

    checkout to id from database

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
+const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cors());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3msfl.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 client.connect(err => {
-    const bookCollection = client.db(`${process.env.DB_NAME}`).collection(`${process.env.DB_COL}`);
+    const bookCollection = client.db("BookShopDb").collection("books");
 
     app.get('/books', (req, res)=>{
-        bookCollection.find()
+        bookCollection.find({})
         .toArray((err, items)=>{
             res.send(items);
             
         })
     })
+    
+    app.get('/books/:id', (req, res)=>{
+        bookCollection.find({_id: ObjectId(req.params.id)})
+        .toArray((err, documents)=>{
+            res.send(documents[0]);
+        })  
+    })
+    
+   
 
     app.post('/addBook', (req, res) => {
         const newEvent = req.body;
